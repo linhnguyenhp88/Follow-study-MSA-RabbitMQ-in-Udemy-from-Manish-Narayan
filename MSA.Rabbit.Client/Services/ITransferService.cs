c4e8985f77c5a91077181b85d10fad76c505afa4
Linh Khanh Nguyen <linhnguyenhp88@gmail.com>
2019-12-17 23:14:58 +0700

    Completed course.

---
+++ MSA.Rabbit.Client/Services/ITransferService.cs
@@ -0,0 +1,13 @@
+﻿using MSA.Rabbit.Client.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MSA.Rabbit.Client.Services
+{
+    public interface ITransferService
+    {
+        Task Transfer(TransferDto transferDto);
+    }
+}
---
+++ MSA.Rabbit.Client/Services/TransferService.cs
@@ -0,0 +1,30 @@
+﻿using MSA.Rabbit.Client.Models.DTO;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSA.Rabbit.Client.Services
+{
+    public class TransferService : ITransferService
+    {
+        private readonly HttpClient _httpClient;
+        private string uri  = "https://localhost:5001/api/Banking";
+
+        public TransferService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task Transfer(TransferDto transferDto)
+        {
+            var transferContent = new StringContent(JsonConvert.SerializeObject(transferDto),
+                                                       Encoding.UTF8, "application/json");
+            var response = await _httpClient.PostAsync(uri, transferContent);
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
